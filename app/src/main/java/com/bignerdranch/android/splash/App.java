d553e4d3750be2edc0287637b03054eb4ba1049a
Chris Stewart <cstewart@bignerdranch.com>
2015-07-17 07:20:25 -0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/splash/App.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.splash;
+
+import android.app.Application;
+import android.os.SystemClock;
+
+import java.util.concurrent.TimeUnit;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Don't do this! This is just so cold launches take some time
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(3));
+    }
+}
